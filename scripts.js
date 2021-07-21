f9ee4d0739de1f2228b39b1c5204302565964f31
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-07-21 11:40:58 +0200

    start game view, puzzle view

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
     const dropBoxes = document.querySelectorAll('.dropBox');
     const pieces = document.querySelectorAll('.pieces');
+    const piecesContainer = document.querySelector('.container-pieces');
+    const puzzle = document.querySelector('.puzzle');
+    const startButton = document.querySelector('.start-btn');
+    const newGameButton = document.querySelector('.newgame-btn');
+    const startView = document.querySelector('.start');
 
+
+    //start view
+
+    puzzle.classList.add('hide');
+    newGameButton.classList.add('hide');
+
+    const startGame = () => {
+        startView.classList.add('hide');
+        puzzle.classList.remove('hide');
+        newGameButton.classList.remove('hide');
+    };
+
+    startButton.addEventListener('click', startGame);
+
+    //The Fisher-Yates algorith to shuffle the puzzle pieces
+    // for (let i = piecesContainer.length; i >= 0; i--) {
+    //     piecesContainer.appendChild(piecesContainer.children[Math.random() * i | 0]);
+    // };
+    
+    
+    //drag and drop
     const drag = event => {
         event.dataTransfer.setData("text", event.target.id);
     };
@@ -12,14 +38,9 @@
         const data = event.dataTransfer.getData("text");
         console.log(data);
         event.target.appendChild(document.getElementById(data));
-    }
+    };
 
-    for(let i=0; i < dropBoxes.length; i++) {
-        dropBoxes[i].addEventListener('ondrop', drop);
-        dropBoxes[i].addEventListener('ondragover', allowDrop);
-    }
+  
 
-    for(let i=0; i < pieces.length; i++) {
-        pieces[i].addEventListener('ondragstart', drag);
-    }
+      
 
