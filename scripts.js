fc0775fe4a86cdb64c3ecc903993208c7da1d700
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-07-22 13:58:05 +0200

    no dropping if dropbox has already a child, check corectness

--- scripts.js
+++ scripts.js
@@ -1,46 +1,67 @@
-    const dropBoxes = document.querySelectorAll('.dropBox');
-    const pieces = document.querySelectorAll('.pieces');
-    const piecesContainer = document.querySelector('.container-pieces');
-    const puzzle = document.querySelector('.puzzle');
-    const startButton = document.querySelector('.start-btn');
-    const newGameButton = document.querySelector('.newgame-btn');
-    const startView = document.querySelector('.start');
+const dropBoxes = document.querySelectorAll('.dropBox');
+const pieces = document.querySelectorAll('.pieces');
+const piecesContainer = document.querySelector('.container-pieces');
+const puzzle = document.querySelector('.puzzle');
+const dropZone = document.querySelector('.dropzone');
+const startButton = document.querySelector('.btn.start-btn');
+const newGameButton = document.querySelector('.newgame-btn');
+const startView = document.querySelector('.start');
 
+const hide = (el) => el.style.visibility = 'hidden';
+const show = (el) => el.style.visibility = 'visible';
+const shuffling = () => {
+  const pieces = piecesContainer.children;
+  const frag = document.createDocumentFragment();
 
-    //start view
+  while (pieces.length) {
+    frag.appendChild(pieces[Math.floor(Math.random() * pieces.length)])
+  };
+  piecesContainer.appendChild(frag);
+};
 
-    puzzle.classList.add('hide');
-    newGameButton.classList.add('hide');
+//start view
+hide(puzzle);
+hide(newGameButton);
 
-    const startGame = () => {
-        startView.classList.add('hide');
-        puzzle.classList.remove('hide');
-        newGameButton.classList.remove('hide');
-    };
+const startGame = () => {
+  hide(startView);
+  show(puzzle)
+  show(newGameButton);
+  //shuffling();
+}
 
-    startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', startGame);
 
-    //The Fisher-Yates algorith to shuffle the puzzle pieces
-    // for (let i = piecesContainer.length; i >= 0; i--) {
-    //     piecesContainer.appendChild(piecesContainer.children[Math.random() * i | 0]);
-    // };
-    
-    
-    //drag and drop
-    const drag = event => {
-        event.dataTransfer.setData("text", event.target.id);
-    };
+//drag and drop
 
-    const allowDrop = event => event.preventDefault();
+const drag = event => {
+  event.dataTransfer.setData('text', event.target.id);
+};
 
-    const drop = event => {
-        event.preventDefault();
-        const data = event.dataTransfer.getData("text");
-        console.log(data);
-        event.target.appendChild(document.getElementById(data));
-    };
+const allowDrop = event => {
+ if (event.target.tagName !== 'IMG') {
+   event.preventDefault();
+  } 
+};
 
+const arrOfIds = [];
+
+const drop = event => {
+  event.preventDefault();
+  const data = event.dataTransfer.getData('text');
+  event.target.appendChild(document.getElementById(data));
+  if (event.target.dataset.boxid === event.target.firstChild.dataset.pieceid) {
+    event.target.style.border = 'none';
+    dropZone.style.border = 'none';
+    arrOfIds.push(event.target.dataset.boxid);
+    if (arrOfIds.length === dropBoxes.length) {
+      alert('you won');
+    }
+  }
   
+}
 
-      
+
+
+
 
